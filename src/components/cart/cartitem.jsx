5e2ac38b93eef67544dd30ac8963ef3b33da73ed
PhucNguyen08 <npnguyen812@gmail.com>
2023-04-16 11:08:23 +0700

    update

--- src/components/cart/cartitem.jsx
+++ src/components/cart/cartitem.jsx
@@ -10,96 +10,137 @@ import { cartActions } from '../store/cart-slice';
 const cx = classNames.bind(styles);
 
 const CartItem = props => {
-  const router = useRouter();
-  const listProducts = useSelector(state => state.cart.items);
-  const totalAmount = useSelector(state => state.cart.totalAmount);
-  const dispatch = useDispatch();
+    const router = useRouter();
+    const listProducts = useSelector(state => state.cart.items);
+    console.log(listProducts);
+    const totalAmount = useSelector(state => state.cart.totalAmount);
+    const dispatch = useDispatch();
 
-  const handleIncreQuantity = product => {
-    dispatch(
-      cartActions.addQuantityItemToCart({
-        id: product.id,
-        price: product.price,
-      })
-    );
-  };
+    const handleIncreQuantity = product => {
+        dispatch(
+            cartActions.addQuantityItemToCart({
+                id: product.id,
+                price: product.price,
+                size: product.size,
+            })
+        );
+    };
 
-  const handleDecreaseQuantity = id => {
-    dispatch(cartActions.removeQuantityItemToCart(id));
-  };
+    const handleDecreaseQuantity = item => {
+        dispatch(
+            cartActions.removeQuantityItemToCart({
+                id: item.id,
+                size: item.size,
+            })
+        );
+    };
 
-  const handleRemoveItem = id => {
-    dispatch(cartActions.removeItemToCart(id));
-  };
+    const handleRemoveItem = item => {
+        dispatch(
+            cartActions.removeItemToCart({ id: item.id, size: item.size })
+        );
+    };
 
-  const handleChangeInput = (id, value) => {
-    dispatch(cartActions.changeQuanityToCart({ id: id, quantity: +value }));
-  };
+    const handleChangeInput = item => {
+        dispatch(
+            cartActions.changeQuanityToCart({
+                id: item.id,
+                quantity: +item.quantity,
+                size: item.size,
+            })
+        );
+    };
 
-  return (
-    <>
-      <div className={cx('wrapper')}>
-        {listProducts.map(item => (
-          <div className={cx('row')} key={item.id}>
-            <div className={cx('product')}>
-              <Link href={`/products/${item.id}`} className={cx('image')}>
-                <img src={item.image} alt={item.name} />
-              </Link>
-              <div className={cx('detail')}>
-                <div className={cx('name')}>
-                  <h4 className={cx('title')}>{item.name}</h4>
-                  <span className={cx('size')}>{item.size}</span>
-                  <button
-                    onClick={() => handleRemoveItem(item.id)}
-                    className={cx('btn-delete')}
-                  >
-                    Xóa
-                  </button>
-                </div>
+    return (
+        <>
+            <div className={cx('wrapper')}>
+                {listProducts.map(item => (
+                    <div className={cx('row')} key={item.id}>
+                        <div className={cx('product')}>
+                            <Link
+                                href={`/products/${item.id}`}
+                                className={cx('image')}
+                            >
+                                <img src={item.image} alt={item.name} />
+                            </Link>
+                            <div className={cx('detail')}>
+                                <div className={cx('name')}>
+                                    <h4 className={cx('title')}>{item.name}</h4>
+                                    <span className={cx('size')}>
+                                        {item.size}
+                                    </span>
+                                    <button
+                                        onClick={() =>
+                                            handleRemoveItem({
+                                                id: item.id,
+                                                size: item.size,
+                                            })
+                                        }
+                                        className={cx('btn-delete')}
+                                    >
+                                        Xóa
+                                    </button>
+                                </div>
 
-                <div className={cx('initial-price')}>
-                  <span>{formatter.format(item.price)}</span>
-                </div>
+                                <div className={cx('initial-price')}>
+                                    <span>{formatter.format(item.price)}</span>
+                                </div>
 
-                <div className={cx('actions')}>
-                  <InputNumber
-                    cart={true}
-                    quantityInput={item.quantity}
-                    handleQuantityInput={e =>
-                      handleChangeInput(item.id, e.target.value)
-                    }
-                    handleInputMinus={() => handleDecreaseQuantity(item.id)}
-                    handleInputPlus={() =>
-                      handleIncreQuantity({
-                        id: item.id,
-                        price: item.price,
-                      })
-                    }
-                  />
-                </div>
-                <div className={cx('total-price')}>
-                  <span>{formatter.format(item.totalPrice)}</span>
-                </div>
-              </div>
+                                <div className={cx('actions')}>
+                                    <InputNumber
+                                        cart={true}
+                                        quantityInput={item.quantity}
+                                        handleQuantityInput={e =>
+                                            handleChangeInput({
+                                                id: item.id,
+                                                quantity: e.target.value,
+                                                size: item.size,
+                                            })
+                                        }
+                                        handleInputMinus={() =>
+                                            handleDecreaseQuantity({
+                                                id: item.id,
+                                                size: item.size,
+                                            })
+                                        }
+                                        handleInputPlus={() =>
+                                            handleIncreQuantity({
+                                                id: item.id,
+                                                price: item.price,
+                                                size: item.size,
+                                            })
+                                        }
+                                    />
+                                </div>
+                                <div className={cx('total-price')}>
+                                    <span>
+                                        {formatter.format(item.totalPrice)}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
-      <div className={cx('pay')}>
-        <div className={cx('pay__total')}>
-          <div className={cx('pay__total-price')}>
-            <span>Tổng tiền:</span>
-            <span className={cx('price')}>{formatter.format(totalAmount)}</span>
-          </div>
-          <div className={cx('pay__total-btn')}>
-            <button onClick={() => router.push('/user/cart/checkout')}>
-              Thanh Toán
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+            <div className={cx('pay')}>
+                <div className={cx('pay__total')}>
+                    <div className={cx('pay__total-price')}>
+                        <span>Tổng tiền:</span>
+                        <span className={cx('price')}>
+                            {formatter.format(totalAmount)}
+                        </span>
+                    </div>
+                    <div className={cx('pay__total-btn')}>
+                        <button
+                            onClick={() => router.push('/user/cart/checkout')}
+                        >
+                            Thanh Toán
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default CartItem;
--- src/components/store/cart-slice.js
+++ src/components/store/cart-slice.js
@@ -1,104 +1,110 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  items: [],
-  totalAmount: 0,
-  totalQuantity: 0,
-  isCart: false,
+    items: [],
+    totalAmount: 0,
+    totalQuantity: 0,
+    isCart: false,
 };
 
 const cartSlice = createSlice({
-  name: 'cart',
-  initialState,
-  reducers: {
-    addItemToCart(state, action) {
-      const newItem = action.payload;
-      const existingItem = state.items.find(
-        item => item.id === newItem.id && item.size === newItem.size
-      );
-      if (existingItem) {
-        existingItem.quantity = existingItem.quantity + newItem.quantity;
-        existingItem.totalPrice += newItem.price * newItem.quantity;
-      } else {
-        state.items.push({
-          id: newItem.id,
-          name: newItem.name,
-          size: newItem.size,
-          price: newItem.price,
-          quantity: newItem.quantity,
-          image: newItem.image,
-          totalPrice: newItem.price * newItem.quantity,
-        });
-      }
-      state.totalQuantity = state.totalQuantity + newItem.quantity;
-      state.totalAmount = state.totalAmount + newItem.price * newItem.quantity;
-      state.isCart = true;
+    name: 'cart',
+    initialState,
+    reducers: {
+        addItemToCart(state, action) {
+            const newItem = action.payload;
+            const existingItem = state.items.find(
+                item => item.id === newItem.id && item.size === newItem.size
+            );
+            if (existingItem) {
+                existingItem.quantity =
+                    existingItem.quantity + newItem.quantity;
+                existingItem.totalPrice += newItem.price * newItem.quantity;
+            } else {
+                state.items.push({
+                    id: newItem.id,
+                    name: newItem.name,
+                    size: newItem.size,
+                    price: newItem.price,
+                    quantity: newItem.quantity,
+                    image: newItem.image,
+                    totalPrice: newItem.price * newItem.quantity,
+                });
+            }
+            state.totalQuantity = state.totalQuantity + newItem.quantity;
+            state.totalAmount =
+                state.totalAmount + newItem.price * newItem.quantity;
+            state.isCart = true;
+        },
+        addQuantityItemToCart(state, action) {
+            const newItem = action.payload;
+            state.totalAmount += newItem.price;
+            state.totalQuantity++;
+            const existingItem = state.items.find(
+                item => item.id === newItem.id && item.size === newItem.size
+            );
+            existingItem.quantity++;
+            existingItem.totalPrice += newItem.price;
+        },
+        removeQuantityItemToCart(state, action) {
+            const newItem = action.payload;
+            console.log(newItem);
+            state.totalQuantity--;
+            const existingItem = state.items.find(
+                item => item.id === newItem.id && item.size === newItem.size
+            );
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter(
+                    item => item.id !== newItem.id || item.size !== newItem.size
+                );
+                if (state.items.length === 0) {
+                    state.isCart = false;
+                }
+            } else {
+                state.totalAmount -= existingItem.price;
+                existingItem.quantity--;
+                existingItem.totalPrice =
+                    existingItem.totalPrice - existingItem.price;
+            }
+        },
+        removeItemToCart(state, action) {
+            const newItem = action.payload;
+            // const id = action.payload;
+            const existingItem = state.items.find(
+                item => item.id === newItem.id && item.size === newItem.size
+            );
+            state.totalAmount -= existingItem.totalPrice;
+            state.totalQuantity -= existingItem.quantity;
+            state.items = state.items.filter(
+                item => item.id !== newItem.id || item.size !== newItem.size
+            );
+            if (state.items.length === 0) {
+                state.isCart = false;
+            }
+        },
+        changeQuanityToCart(state, action) {
+            const newItem = action.payload;
+            const existingItem = state.items.find(
+                item => item.id === newItem.id && item.size === newItem.size
+            );
+            existingItem.quantity = newItem.quantity;
+            existingItem.totalPrice = newItem.quantity * existingItem.price;
+            state.totalAmount = state.items.reduce(
+                (sum, item) => sum + item.totalPrice,
+                0
+            );
+            state.totalQuantity = state.items.reduce(
+                (sum, item) => sum + item.quantity,
+                0
+            );
+        },
+        resetCart(state) {
+            state.items = [];
+            state.totalQuantity = 0;
+            state.totalPrice = 0;
+            state.isCart = false;
+        },
     },
-    addQuantityItemToCart(state, action) {
-      const newItem = action.payload;
-      state.totalAmount += newItem.price;
-      state.totalQuantity++;
-      const existingItem = state.items.find(
-        item => item.id === newItem.id && item.size === newItem.size
-      );
-      existingItem.quantity++;
-      existingItem.totalPrice += newItem.price;
-    },
-    removeQuantityItemToCart(state, action) {
-      const id = action.payload;
-      state.totalQuantity--;
-      const existingItem = state.items.find(
-        item => item.id === id && item.size === newItem.size
-      );
-      if (existingItem.quantity === 1) {
-        state.items = state.items.filter(
-          item => item.id !== id || item.size !== newItem.size
-        );
-        if (state.items.length === 0) {
-          state.isCart = false;
-        }
-      } else {
-        state.totalAmount -= existingItem.price;
-        existingItem.quantity--;
-        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
-      }
-    },
-    removeItemToCart(state, action) {
-      const id = action.payload;
-      const existingItem = state.items.find(
-        item => item.id === id && item.size === newItem.size
-      );
-      state.totalAmount -= existingItem.totalPrice;
-      state.totalQuantity -= existingItem.quantity;
-      state.items = state.items.filter(item => item.id !== id);
-      if (state.items.length === 0) {
-        state.isCart = false;
-      }
-    },
-    changeQuanityToCart(state, action) {
-      const id = action.payload.id;
-      const quantityItem = action.payload.quantity;
-      const existingItem = state.items.find(
-        item => item.id === id && item.size === newItem.size
-      );
-      existingItem.quantity = quantityItem;
-      existingItem.totalPrice = quantityItem * existingItem.price;
-      state.totalAmount = state.items.reduce(
-        (sum, item) => sum + item.totalPrice,
-        0
-      );
-      state.totalQuantity = state.items.reduce(
-        (sum, item) => sum + item.quantity,
-        0
-      );
-    },
-    resetCart(state) {
-      state.items = [];
-      state.totalQuantity = 0;
-      state.totalPrice = 0;
-      state.isCart = false;
-    },
-  },
 });
 
 export const cartActions = cartSlice.actions;
